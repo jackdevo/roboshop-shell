2dfa0a87ec32af5bd25e0a217d7224b1fcacf69f
Jack <jackinbbb@gmail.com>
2022-09-14 19:24:41 +0530

    Add ALL component Scripts

--- cart.sh
+++ cart.sh
@@ -1,85 +1,47 @@
+
+StatusCheck() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 1
+  fi
+}
 echo Setiing NodeJS repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Installing NodeJS
 yum install nodejs -y &>>tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
 
 id roboshop &>>/tmp/cart.log
 if [ $? -ne 0 ]; then
   echo Adding Application User
   useradd roboshop &>>tmp/cart.log
-   if [ $? -eq 0 ]; then
-     echo -e "\e[32mSUCCESS\e[0m"
-   else
-     echo -e "\e[31mFAILURE\e[0m"
-     exit 1
-   fi
+  StatusCheck
 fi
 
 echo Downloading Application Content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>tmp/cart.log
 cd /home/roboshop &>>tmp/cart.log
-
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Cleaning old Application Content
-rm -rf cart &>>tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Extract Application Archive
 unzip -o /tmp/cart.zip &>>tmp/cart.log && mv cart-main cart &>>tmp/cart.log && cd cart &>>tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Installing NodeJs Dependencies
 npm install &>>tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Configuring Cart SystemD Service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>tmp/cart.log && systemctl daemon-reload &>>tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
 
 echo Starting Cart Service
 systemctl start cart &>>tmp/cart.log && systemctl enable cart &>>tmp/cart.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatusCheck
--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,20 @@
+StatusCheck() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 1
+  fi
+}
+
+echo "COnfiguring NodeJS yum Repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+StatusCheck
+
+echo Installing NodeJS
 yum install nodejs -y
+StatusCheck
+
 useradd roboshop
 
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
